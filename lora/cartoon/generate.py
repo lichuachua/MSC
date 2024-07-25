b2af36d9cf0a5ebf76d943f0fcbefe0931664026
lichuachua <1065618302@qq.com>
2024-07-25 16:34:29 +0100

    lora cartoon init

---
+++ lora/cartoon/generate.py
@@ -0,0 +1,7 @@
+from diffusers import AutoPipelineForText2Image
+import torch
+
+pipeline = AutoPipelineForText2Image.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16).to("cuda")
+pipeline.load_lora_weights("./fine-tune-lora", weight_name="pytorch_lora_weights.safetensors")
+image = pipeline("A pokemon with blue eyes").images[0]
+image.save("pokemon.png")
