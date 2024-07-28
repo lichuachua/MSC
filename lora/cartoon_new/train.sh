1aee091fd57ee3397ca6b23839e99c347c69b290
lichuachua <1065618302@qq.com>
2024-07-28 23:08:23 +0100

    cartoon new

---
+++ lora/cartoon_new/train.sh
@@ -0,0 +1,40 @@
+# single core submission script
+
+#Run with current environment (-V) and in the current directory (-cwd)
+#$ -V -cwd
+
+#Request some time- min 15 mins - max 48 hours
+#$ -l h_rt=40:00:00
+
+#Request some memory per core
+#$ -l h_vmem=2G
+
+#Get email at start and end of the job
+#$ -m be
+
+#$ -l coproc_v100=2
+
+#Now run the job
+module load cuda
+nvidia-smi
+
+
+python ../diffusers/examples/text_to_image/train_text_to_image_lora.py \
+  --pretrained_model_name_or_path="runwayml/stable-diffusion-v1-5"  \
+  --dataset_name="./cartoon_dataset" \
+  --dataloader_num_workers=8 \
+  --resolution=512 \
+  --center_crop \
+  --random_flip \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --max_train_steps=1500 \
+  --learning_rate=1e-04 \
+  --max_grad_norm=1 \
+  --lr_scheduler="cosine" \
+  --lr_warmup_steps=0 \
+  --output_dir="./fine-tune-lora" \
+  --hub_model_id="cartoon-lora" \
+  --checkpointing_steps=500 \
+  --validation_prompt="a girl in a red dress standing on a wooden fence" \
+  --seed=1337
